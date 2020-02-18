172369930289a5a011b43c5748959339499b4db1
Jeremy Coando <jaycoan@yahoo.com>
2020-02-17 18:18:52 -0700

    added another exercise

--- number_exercise.js
+++ number_exercise.js
@@ -55,22 +55,75 @@
 //     to see what the completed output should look like
 
 
-// <script src="js/math.js"></script>
-alert('Lets do some Math!');
+
+
+
+<script src="js/math.js"></script>
+alert('Lets\' do some math!');
 var num1 = prompt('Please enter a number');
 num1 = parseFloat(num1);
 var num2 = prompt('Please enter another number');
 num2 = parseFloat(num2);
-var message = "<h1>Math with these numbers " + num1 + " and " + num2 + "</h1>";
-message += ("<br>")
+var message = "<h1>Math with the numbers " + num1 + " and " + num2 +  "</h1>";
 message += num1 + " + " + num2 + " = " + (num1 + num2);
-message += + ("<br>")
-message += num1 + " / " + num2 + " = " + (num1/num2);
-message += ("<br>")
-message += num1 + " - " + num2 + " = " + (num1-num2);
-message += ("<br>")
-message += num1 + " * " + num2 + " = " + (num1*num2);
+message += ("<br>");
+message += num1 + " * " + num2 + " = " + (num1 * num2);
+message += ("<br>");
+message += num1 + " / " + num2 + " = " + (num1 / num2);
+message += ("<br>");
+message += num1 + " - " + num2 + " = " + (num1 - num2);
+message += ("<br>");
 document.write(message);
+
+
+// declare program variables
+var num1;
+var num2;
+var message;
+
+// announce the program
+alert("Let's do some math!");
+
+// collect numeric input
+num1 = prompt("Please type a number");
+num1 = parseFloat(num1);
+num2 = prompt("Please type another number");
+num2 = parseFloat(num2);
+
+if (num2 == 0) {
+    alert("The second number is 0. You can't divide by zero. Reload and try again.");
+} else if (isNaN(num1) || isNaN(num2) ){
+    alert("At least one of the values you typed is not a number. Reload and try again.");
+
+} else {
+
+    // build an HTML message
+    message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
+    message += num1 + " + " + num2 + " = " + (num1 + num2);
+    message += "<br>";
+    message += num1 + " * " + num2 + " = " + (num1 * num2);
+    message += "<br>";
+    message += num1 + " / " + num2 + " = " + (num1 / num2);
+    message += "<br>";
+    message += num1 + " - " + num2 + " = " + (num1 - num2);
+    
+    // write message to web page
+    document.write(message);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
